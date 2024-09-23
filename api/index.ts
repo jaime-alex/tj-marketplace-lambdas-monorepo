40dd3451c4263379c97997093b5e6f7cd6017d80
jaime-alex <jaime-alex@outlook.com>
2024-09-22 20:42:43 -0700

    change index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,8 @@
-require('dotenv').config();
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const bodyParser = require('body-parser');
-const path = require('path');
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', 'components', 'home.htm'));
-});
-
-app.get('/about', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', 'components', 'about.htm'));
-});
-
-app.get('/uploadUser', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', 'components', 'user_upload_form.htm'));
-});
-
-app.listen(3000, () => console.log('Server ready on port 3000.'));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
