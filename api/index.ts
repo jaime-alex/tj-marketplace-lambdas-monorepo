b2a879048b95d5d278ad54eaf273a41c60d2f629
jaime-alex <jaime-alex@outlook.com>
2024-09-22 20:37:59 -0700

    add express

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,28 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+require('dotenv').config();
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello to the tj marketplace products api`,
-  })
-}
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'components', 'home.htm'));
+});
+
+app.get('/about', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'components', 'about.htm'));
+});
+
+app.get('/uploadUser', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'components', 'user_upload_form.htm'));
+});
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+module.exports = app;
