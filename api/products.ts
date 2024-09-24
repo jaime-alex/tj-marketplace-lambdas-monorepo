b9eb912192495193eeffb84a33f0ff40ecfdfb28
jaime-alex <jaime-alex@outlook.com>
2024-09-23 23:07:05 -0700

    add mongo db connection

--- api/products.ts
+++ api/products.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import {run} from "../services/mongo-db";
 var router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    await run().then(res => {
+        console.log("success")
+    }).catch((err: any) => {
+        console.error("error", err)
+    })
     res.send('Birds home page')
 })
 
