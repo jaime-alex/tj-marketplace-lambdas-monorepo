27b9e479978a5d181b00830c0f23506ee25eabc1
jaime-alex <jaime-alex@outlook.com>
2024-10-05 22:39:12 -0700

    Separate logics

--- src/lambda-functions/product-functions/products-handler.js
+++ src/lambda-functions/product-functions/products-handler.js
@@ -1,11 +1,10 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const compression = require('compression')
+const {getAllProducts} = require('./routes/all-products')
 
 const {
   DynamoDBDocumentClient,
-  GetCommand,
   PutCommand,
-  QueryCommand,
   ScanCommand
 } = require("@aws-sdk/lib-dynamodb");
 
@@ -26,38 +25,7 @@ app.get("/products/data/info", (_, res) => {
   res.status(200).json({ message: "Products endpoint information." });
 })
 
-app.get("/products", async (req, res) => {
-  const { LastEvaluatedKey } = req.query;  // Extract the LastEvaluatedKey from query parameters (if exists)
-  
-  let params = {
-    TableName: 'tj-market-products',
-    IndexName: 'title-enabled-index',
-    Limit: 20
-  };
-
-  // If the request contains LastEvaluatedKey, add it to params for pagination
-  if (LastEvaluatedKey) {
-    params.ExclusiveStartKey = JSON.parse(LastEvaluatedKey);  // Parse the stringified LastEvaluatedKey
-  }
-
-  try {
-    const command = new ScanCommand(params);
-    const response = await client.send(command);
-
-    if (response) {
-      // Send response along with the LastEvaluatedKey for client to fetch the next page if necessary
-      res.json({
-        Items: response.Items,
-        LastEvaluatedKey: response.LastEvaluatedKey ? JSON.stringify(response.LastEvaluatedKey) : null
-      });
-    } else {
-      res.status(404).json({ error: 'No products found' });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Could not retrieve products" });
-  }
-});
+app.get("/products", getAllProducts);
 
 app.get("/products/:title", async (req, res) => {
   console.log("title param: ", req.params.title)
