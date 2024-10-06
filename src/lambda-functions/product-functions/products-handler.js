3dcf523b2e0a5dd8064baf920055a6064c349e36
jaime-alex <jaime-alex@outlook.com>
2024-10-05 22:57:57 -0700

    try fix

--- src/lambda-functions/product-functions/products-handler.js
+++ src/lambda-functions/product-functions/products-handler.js
@@ -1,9 +1,9 @@
 const compression = require('compression')
-const express = require("express")
-const serverless = require("serverless-http")
-
 const {getAllProducts} = require('./routes/all-products')
 const {searchProducts} = require('./routes/search-products')
+
+const express = require("express");
+const serverless = require("serverless-http");
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(express.json());
 app.get("/products", getAllProducts);
 app.get("/products/:title", searchProducts);
 
-app.use((_, res, _) => {
+app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
   });
